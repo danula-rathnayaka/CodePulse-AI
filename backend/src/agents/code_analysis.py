4a295f668826aa28869682a244b616c459697add
Danula <danusathmin@gmail.com>
2025-02-12 19:49:20 +0530

    Implement folder code analysis api

--- backend/src/agents/code_analysis.py
+++ backend/src/agents/code_analysis.py
@@ -150,52 +150,16 @@ def get_code_review_for_file(file_path: str) -> dict:
         )
     )
 
-    output_dir = "../outputs"
-    os.makedirs(output_dir, exist_ok=True)
-
-    for file, feedback in result['report'].items():
-        file_name = os.path.basename(file)
-        output_path = os.path.join(output_dir, f"{file_name}-code-analysis.md")
-
-        with open(output_path, "w", encoding="utf-8") as f:
-            f.write(feedback)
-
     return dict(result['report'])
 
-# if __name__ == "__main__":
-#     file_or_directory = input("Enter a file path or project directory path: ").strip()
-#
-#     if os.path.isfile(file_or_directory):
-#         file_path = file_or_directory
-#         project_path = ""
-#         ignore_files = []
-#         file_extensions = []
-#     else:
-#         file_path = ""
-#         project_path = file_or_directory
-#         ignore_files = input(
-#             "Enter the files to exclude from scanning (separated by spaces, leave empty for none): ").split()
-#         file_extensions = input(
-#             "Enter the file extensions to scan (e.g., .py .txt, leave empty for all files): ").split()
-#
-#     result = code_review_executor.invoke(
-#         CodeReviewState(
-#             file_path=file_path,
-#             project_path=project_path,
-#             ignore_files=ignore_files,
-#             file_extensions=file_extensions
-#         )
-#     )
-#
-#     output_dir = "../outputs"
-#     os.makedirs(output_dir, exist_ok=True)
-#
-#     for file, feedback in result['report'].items():
-#         file_name = os.path.basename(file)
-#         output_path = os.path.join(output_dir, f"{file_name}-code-analysis.md")
-#
-#         with open(output_path, "w", encoding="utf-8") as f:
-#             f.write(feedback)
-#
-#         absolute_path = os.path.abspath(output_path)
-#         print(f"✅ Code analysis report for file {file_name} saved at: {absolute_path}")
+def get_code_review_for_folder(project_path: str, ignore_files, file_extensions) -> dict:
+    result = code_review_executor.invoke(
+        CodeReviewState(
+            file_path="",
+            project_path=project_path,
+            ignore_files=ignore_files,
+            file_extensions=file_extensions
+        )
+    )
+
+    return dict(result['report'])
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Form
 
-from agents.code_analysis import get_code_review_for_file
+from agents.code_analysis import get_code_review_for_file, get_code_review_for_folder
+from agents.bug_fixer import get_bug_fixer
 
 app = FastAPI(title="CodePulse AI", version="1.0")
 
 
-@app.post("/review_file")
-async def review_uploaded_file(file_path: str) -> dict:
+@app.get("/review_file")
+async def review_file(file_path: str) -> dict:
     return {"review": get_code_review_for_file(file_path)}
 
+
+@app.get("/review_folder")
+async def review_folder(project_path: str, ignore_files, file_extensions) -> dict:
+    return {"review": get_code_review_for_folder(project_path, ignore_files, file_extensions)}
+
+
+@app.get("/bug_fixer")
+async def bug_fixer(file_path: str, error_msg: str) -> dict:
+    return {"review": get_bug_fixer(file_path, error_msg)}
